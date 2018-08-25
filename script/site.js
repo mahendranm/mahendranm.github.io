f46674f7eb4a53bd87f692a349d3134473480788
mahendranm <mahendranec@hotmail.com>
2018-08-25 15:20:24 +0530

    added script file

---
+++ script/site.js
@@ -0,0 +1,58 @@
+function validateForm() {
+    var name =  document.getElementById('name').value;
+    if (name == "") {
+        document.getElementById('status').innerHTML = "Name cannot be empty";
+        return false;
+    }
+    var email =  document.getElementById('email').value;
+    if (email == "") {
+        document.getElementById('status').innerHTML = "Email cannot be empty";
+        return false;
+    } else {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!re.test(email)){
+            document.getElementById('status').innerHTML = "Email format invalid";
+            return false;
+        }
+    }
+    var subject =  document.getElementById('subject').value;
+    if (subject == "") {
+        document.getElementById('status').innerHTML = "Subject cannot be empty";
+        return false;
+    }
+    var message =  document.getElementById('message').value;
+    if (message == "") {
+        document.getElementById('status').innerHTML = "Message cannot be empty";
+        return false;
+    }
+    document.getElementById('status').innerHTML = "Sending...";
+   sendEmail();
+
+}
+function sendEmail(){
+emailModel = {
+    'name'     : $('input[name=name]').val(),
+    'email'    : $('input[name=email]').val(),
+    'subject'  : $('input[name=subject]').val(),
+    'message'  : $('textarea[name=message]').val()
+};
+
+
+$.ajax({
+url : "http://mahendranm.azurewebsites.net/api/emailsender/sendemail",
+type: "POST",
+data : emailModel,
+crossDomain : true,
+success: function(data, textStatus, jqXHR)
+{
+if (data=="OK"){
+    $('#status').text("Mail sent successfully!");
+    $('#contact-form').closest('form').find("input[type=text], textarea").val("");
+	}
+},
+error: function (jqXHR, textStatus, errorThrown)
+{
+    $('#status').text("Error occured while sending email.");
+}
+});
+}
